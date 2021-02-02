cdf15ffaafe93ca408ee41090ae18470e867fb88
rit-bit <71881973+rit-bit@users.noreply.github.com>
2021-02-02 18:02:09 +0000

    Add chips

--- game.js
+++ game.js
@@ -1,26 +1,49 @@
+// IRON
 const ironAmount = document.querySelector('#iron-amount');
-const fuelAmount = document.querySelector('#fuel-amount');
 const ironIncome = document.querySelector('#iron-income');
+// FUEL
+const fuelAmount = document.querySelector('#fuel-amount');
 const fuelIncome = document.querySelector('#fuel-income');
+// CHIPS
+const chipsAmount = document.querySelector('#chips-amount');
+const chipsIncome = document.querySelector('#chips-income');
+// BUTTONS
 const ironBtn = document.querySelector('#iron-btn');
 const fuelBtn = document.querySelector('#fuel-btn');
+const chipsBtn = document.querySelector('#chips-btn');
 
 let iron = 0;
 let miners = 1;
 let fuel = 0;
 let fuelers = 0;
+let chips = 10;
+let chipMakers = 0;
 
 ironBtn.addEventListener("click", () => {
     if (iron >= 5) {
-        iron -= 5;
-        miners++;
+        if (chips >= 1) {
+            iron -= 5;
+            chips -= 1;
+            miners++;
+        }
     }
 });
 
 fuelBtn.addEventListener("click", () => {
     if (iron >= 15) {
-        iron -= 15;
-        fuelers++;
+        if (chips >= 3) {
+            iron -= 15;
+            chips -= 2;
+            fuelers++;
+        }
+    }
+});
+
+chipsBtn.addEventListener("click", () => {
+    if (iron >= 150) {
+        iron -= 150;
+        chipMakers++;
+        chipsBtn.style.visibility = 'hidden';
     }
 });
 
@@ -29,9 +52,12 @@ window.setInterval(() => {
     fuelAmount.innerHTML = fuel;
     ironIncome.innerHTML = miners;
     fuelIncome.innerHTML = fuelers;
+    chipsAmount.innerHTML = chips;
+    chipsIncome.innerHTML = chipMakers;
 }, 50);
 
 window.setInterval(() => {
     iron += miners;
     fuel += fuelers;
+    chips += chipMakers;
 }, 1000);
